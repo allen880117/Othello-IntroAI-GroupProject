0f4d3e45fe95c0af7c2c8244b309a13d8dbcce4e
allen880117 <wangallen880117@gmail.com>
2020-07-30 16:18:31 +0800

    Upload

--- Othello-Src-Python/uct.py
+++ Othello-Src-Python/uct.py
@@ -12,14 +12,14 @@ COEFF = math.sqrt(2)
 class Node():
     """ Class: Node of UCT """
 
-    def __init__(self, **kwargs):
+    def __init__(self, _parent: Node, _state: board.Board, _step: coord.Coord, _is_black: bool):
         """ Constructor """
-        self.parent = kwargs['parent']
+        self.parent = _parent
         self.childs = []
 
-        self.state = kwargs['state']
-        self.step = kwargs['step']
-        self.is_black = kwargs['is_black']
+        self.state = _state
+        self.step = _step
+        self.is_black = _is_black
 
         self.valid_next_steps = []
         self.update_valid_next_steps()
@@ -53,4 +53,21 @@ class Tree():
         self.weight_white = weight.Weight()
 
     def uct_search(self, _s0: board.Board, _is_black: bool) -> coord.Coord:
-        return coord.NO_ACTION
+        """ 
+            Do Search in UCT 
+            (_is_black is the Last Pieces's Color)    
+        """
+        if (self.root == None):
+            self.root = Node(None, _s0, coord.NO_ACTION, _is_black)
+        else:
+            matched = False
+            child: Node
+            for child in self.root.childs:
+                if child.state == _s0:
+                    self.root = child
+                    self.root.parent = None
+                    matched = True
+                    break
+
+            if not matched:
+                self.root = Node(None, _s0, coord.NO_ACTION, _is_black)
